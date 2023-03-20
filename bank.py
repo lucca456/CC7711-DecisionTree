a6932cdb8f6e6c285e79fb1597374f0b91b5a8d2
João Vitor Lisboa <64211510+mestrelisboa@users.noreply.github.com>
2023-03-20 02:40:02 -0300

    Final commit

--- bank.py
+++ bank.py
@@ -5,43 +5,28 @@ from sklearn.tree import DecisionTreeClassifier
 import matplotlib.pyplot as plt
 from scipy.io import arff
 
-def bank():
-    data,meta = arff.loadarff('./bank.arff')
+data = arff.loadarff('bank.arff')
+df = pd.DataFrame(data[0])
+df['subscribed'] = df['subscribed'].apply(lambda x: x.decode('utf-8')) # converte a coluna subscribed para string
 
-    attributes = meta.names()
-    data_value = np.asarray(data)
+features = df.drop('subscribed', axis=1)
+target = df['subscribed']
 
-    age = np.asarray(data['age']).reshape(-1,1)
-    job = np.asarray(data['job']).reshape(-1,1)
-    marital = np.asarray(data['marital']).reshape(-1,1)
-    education = np.asarray(data['education']).reshape(-1,1)
-    default = np.asarray(data['default']).reshape(-1,1)
-    housing = np.asarray(data['housing']).reshape(-1,1)
-    loan = np.asarray(data['loan']).reshape(-1,1)
-    contact = np.asarray(data['contact']).reshape(-1,1)
-    day = np.asarray(data['day']).reshape(-1,1)
-    month = np.asarray(data['month']).reshape(-1,1)
-    duration = np.asarray(data['duration']).reshape(-1,1)
-    campaign = np.asarray(data['campaign']).reshape(-1,1)
-    pdays = np.asarray(data['pdays']).reshape(-1,1)
-    previous = np.asarray(data['previous']).reshape(-1,1)
-    poutcome = np.asarray(data['poutcome']).reshape(-1,1)
+categorical_columns = ['job', 'marital', 'education', 'default', 'housing', 'loan', 'contact', 'month', 'poutcome']
+for column in categorical_columns:
+    features[column] = pd.Categorical(features[column])
+    features[column] = features[column].cat.codes
 
-    features = np.concatenate((age, loan, marital, education, default, housing, contact, day, month, duration, campaign, pdays, previous, poutcome), axis=1)
-    # features = data
-    target = data['subscribed']
-    Arvore = DecisionTreeClassifier(criterion='entropy').fit(features, target)
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=0.2, random_state=42)
 
-    plt.figure(figsize=(10, 6.5))
+Arvore = DecisionTreeClassifier(criterion='entropy', max_depth=5, random_state=42)
+Arvore.fit(X_train, y_train)
 
-    tree.plot_tree(Arvore,feature_names=['age','loan','marital','education','default','housing','contact', 'day', 'month', 'duration','campaign','pdays','previous', 'poutcome'],class_names=['yes', 'no'],
-                   filled=True, rounded=True)
-    plt.show()
+plt.figure(figsize=(25,10))
+tree.plot_tree(Arvore, feature_names=list(features.columns), class_names=['yes', 'no'], filled=True, rounded=True)
+plt.show()
 
-    fig, ax = plt.subplots(figsize=(25, 10))
-    metrics.plot_confusion_matrix(Arvore,features,target,display_labels=['yes', 'no'], values_format='d', ax=ax)
-    plt.show()
-
-
-
-bank()
+fig, ax = plt.subplots(figsize=(25, 10))
+metrics.plot_confusion_matrix(Arvore, X_test, y_test, display_labels=['yes', 'no'], values_format='d', ax=ax)
+plt.show()
